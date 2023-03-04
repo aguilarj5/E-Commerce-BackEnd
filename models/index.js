922812d744295552f9a245467fedcfce5c82ca9c
Jags <jon.kmjt@gmail.com>
2023-03-04 14:53:14 -0500

    db tables created and seeds was successful

--- models/index.js
+++ models/index.js
@@ -8,13 +8,16 @@ const ProductTag = require('./productTag');
 Product.belongsTo(Category, {
 	foreignKey: 'category_id',
 });
+
 // Categories have many Products
 Category.hasMany(Product, {
 	foreignKey: 'category_id',
 	onDelete: 'CASCADE',
 });
+
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, { through: ProductTag });
+
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, { through: ProductTag });
 
--- seeds/product-seeds.js
+++ seeds/product-seeds.js
@@ -3,32 +3,32 @@ const { Product } = require('../models');
 const productData = [
 	{
 		product_name: 'Plain T-Shirt',
-		price: 14.99,
-		stock: 14,
+		product_price: 14.99,
+		product_stock: 14,
 		category_id: 1,
 	},
 	{
 		product_name: 'Running Sneakers',
-		price: 90.0,
-		stock: 25,
+		product_price: 90.0,
+		product_stock: 25,
 		category_id: 5,
 	},
 	{
 		product_name: 'Branded Baseball Hat',
-		price: 22.99,
-		stock: 12,
+		product_price: 22.99,
+		product_stock: 12,
 		category_id: 4,
 	},
 	{
 		product_name: 'Top 40 Music Compilation Vinyl Record',
-		price: 12.99,
-		stock: 50,
+		product_price: 12.99,
+		product_stock: 50,
 		category_id: 3,
 	},
 	{
 		product_name: 'Cargo Shorts',
-		price: 29.99,
-		stock: 22,
+		product_price: 29.99,
+		product_stock: 22,
 		category_id: 2,
 	},
 ];
